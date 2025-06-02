b1f564916ef8da6b5e070c908d9e8844865ee791
ismael <ismael.paivajr@sptech.school>
2025-06-01 23:29:44 -0300

    página do Seu Perfil finalizada

--- site/public/js/sessao.js
+++ site/public/js/sessao.js
@@ -2,6 +2,7 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var senha = sessionStorage.SENHA_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
@@ -15,12 +16,6 @@ function validarSessao() {
 function limparSessao() {
     sessionStorage.clear();
     window.location = "../login.html";
-}
-
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
 }
 
 function finalizarAguardar(texto) {
